f199aba78547fb03307788b35c65c773043b5487
LCJ106 <45348567+LCJ106@users.noreply.github.com>
2019-03-13 22:22:27 +0800

    旋转数组最小数
    把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。 输入一个非减排序的数组的一个旋转，输出旋转数组的最小元素。 例如数组{3,4,5,1,2}为{1,2,3,4,5}的一个旋转，该数组的最小值为1。 NOTE：给出的所有元素都大于0，若数组大小为0，请返回0。

---
+++ minNum.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+public class Solution {
+    public int minNumberInRotateArray(int [] array) {
+        for(int i=0;i<array.length-1;i++){
+            for(int j=0;j<array.length-1;j++){
+                if(array[j]>array[j+1]){
+                    int c=array[j];
+                    array[j]=array[j+1];
+                    array[j+1]=c;
+                }
+            }
+        }
+        return array[0];
+    
+    }
+}
