1376d0bb5409f14d3c1ed2b118d438e7e6335020
LCJ106 <45348567+LCJ106@users.noreply.github.com>
2019-03-13 22:10:53 +0800

    输入一链表，从尾到头返回一个ArrayList。

---
+++ ListFromTailtoHead.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.*;
+public class Solution {
+    public ArrayList<Integer> printListFromTailToHead(ListNode listNode) {
+        Stack<Integer> stack=new Stack<>();
+        while(listNode!=null){
+            stack.push(listNode.val);
+            listNode=listNode.next;
+        }
+        ArrayList<Integer> list=new ArrayList<>();
+        while(!stack.isEmpty())
+            list.add(stack.pop());
+        return list;
+    }
+}
