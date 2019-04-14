33bc180bcb75630f49cf7831d08feda592caa4fa
LCJ106 <45348567+LCJ106@users.noreply.github.com>
2019-04-14 14:57:24 +0800

    二叉树深度

--- TreeDepth.java
+++ TreeDepth.java
@@ -2,14 +2,8 @@ public class Solution {
     public int TreeDepth(TreeNode root) {
         if(root==null)
             return 0;
-        if(root.left!=null&root.right!=null){
-            return 1+Math.max(TreeDepth(root.left),TreeDepth(root.right));
-        }
-        else if(root.left!=null&root.right==null)
-            return 1+TreeDepth(root.left);
-        else if(root.left==null&root.right!=null)
-            return 1+TreeDepth(root.right);
         else
-            return 1;
+            return 1+Math.max(TreeDepth(root.left),TreeDepth(root.right));
+        
     }
 }
