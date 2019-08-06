5d4308c43969c3b0e9c2ca2c936d46821acf3c25
LCJ106 <45348567+LCJ106@users.noreply.github.com>
2019-08-06 16:11:25 +0800

    约瑟夫环

--- LastRemaining.java
+++ LastRemaining.java
@@ -1,5 +1,5 @@
 public class Solution {
-    public int LastRemaining_Solution(int n, int m) {
+    public int LastRemaining_Solution(int n, int m) { //步长m,总人数n,得到最后一个出队的初始下标
         int x=0;
         if(n==0)
             return -1;
